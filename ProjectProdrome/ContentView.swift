ac4e1ae126d84c8f3a025c80f93e431fcefd7c37
Michael <sawlani.michael@yahoo.com>
2019-10-22 13:10:14 -0400

    Initial Commit

---
+++ ProjectProdrome/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectProdrome
+//
+//  Created by Michael Sawlani on 10/22/19.
+//  Copyright © 2019 Michael Sawlani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
